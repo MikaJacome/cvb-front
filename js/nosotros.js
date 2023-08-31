a87e6c9c73b46f7eec78183bdf3c50a20f998fbc
Carla Micaela Jacome <mika_jacome@hotmail.com>
2023-08-31 23:31:29 +0200

    Finish About us Section

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,5 +1,5 @@
-const nombre = document.querySelector('.nombre h3');
-    nombre.innerHTML = nombre.innerName.split('').map(
-        (char, i) =>
-        `<span style= "transform:rotate(${i * 8.3}deg)">${char}</span>`
-        ).join('');
+const nombre = document.querySelector('.nombre p');
+nombre.innerHTML = nombre.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i*13}deg)">${char}</span>`
+).join("")
